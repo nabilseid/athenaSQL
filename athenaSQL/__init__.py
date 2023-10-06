6f4ac651a0cb7645e90ab1b4256d906fff3f49fb
Nabil Seid <nabeelseid@gmail.com>
2023-10-06 18:12:05 +0300

    Reorder pkg

---
+++ athenaSQL/__init__.py
@@ -0,0 +1,3 @@
+from athenasql.athena import Athena
+from athenaSQL.athena_table import AthenaTable, TempTable
+from athenaSQL.column import Column
--- src/athenaSQL/__init__.py
+++
@@ -1,3 +0,0 @@
-from .athena import Athena 
-from .athena_table import AthenaTable, TempTable
-from .column import Column
