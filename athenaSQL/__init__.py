26432929e60d444ea0533f1c49295f19e1c92c72
Nabil Seid <nabeelseid@gmail.com>
2023-10-22 00:52:28 +0300

    add __version__ from __name__ in __init__

--- athenaSQL/__init__.py
+++ athenaSQL/__init__.py
@@ -1,4 +1,8 @@
+from importlib.metadata import version
+
 from .athena import Athena
 from .athena_table import AthenaTable, TempTable
 from .column import Column
 from .errors import PartialQueryError
+
+__version__ = version(__name__)
