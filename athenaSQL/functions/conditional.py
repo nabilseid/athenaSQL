c2fb5c27eb70a2e45d03999842a1588c57bd48b3
Nabil Seid <nabeelseid@gmail.com>
2023-11-25 19:35:00 +0300

    Add  SQL counterpart  function

--- athenaSQL/functions/conditional.py
+++ athenaSQL/functions/conditional.py
@@ -2,6 +2,16 @@ from athenaSQL.column import Column, NewColumn, \
     ConditionalColumn, CaseColumn, WindowColumn
 
 
+def ifTrue(condition, true_value, false_value=None):
+
+    if not isinstance(condition, Column):
+        raise TypeError(f'{condition} argument is not type of a `Column`')
+    
+    condition._sql_clause = (f'IF({condition}, {true_value}'
+                             f', {false_value})' if false_value != None else ')')
+
+    return condition
+
 def tryOrNull(expression):
     """Evaluate an expression and handle certain types of errors by returning NULL.
     The sql function identifier is `TRY(expression)`.
@@ -25,5 +35,6 @@ def tryOrNull(expression):
 
 # Ensure that Sphinx finds the dynamically created functions
 __all__ = [
+    'ifTrue',
     'tryOrNull'
 ]
