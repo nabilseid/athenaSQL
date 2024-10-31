5d49a158b096483e12255b10de1c97e314a8c8d3
Nabil Seid <nabeelseid@gmail.com>
2024-10-31 15:15:25 +0300

    Fix column orders

--- athenaSQL/orders.py
+++ athenaSQL/orders.py
@@ -1,17 +1,17 @@
 from athenaSQL.column import validate_column_name
 
 
-class ASC:
-    def __init__(self, column):
-        self._column = validate_column_name(column)
-
-    def __str__(self):
-        return f'{self._column} DESC'
-
-
 class DESC:
     def __init__(self, column):
         self._column = validate_column_name(column)
 
     def __str__(self):
-        return f'{self._column} ASC'
+        return f"{self._column} DESC"
+
+
+class ASC:
+    def __init__(self, column):
+        self._column = validate_column_name(column)
+
+    def __str__(self):
+        return f"{self._column} ASC"
